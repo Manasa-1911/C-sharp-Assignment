af58d2708c07217c6369283bd79a87fd1f8874d4
Manasa-1911 <89526344+Manasa-1911@users.noreply.github.com>
2022-08-22 22:26:53 +0530

    Update program 5.cs

--- program 5.cs
+++ program 5.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppDemo
 {
-    internal class ReplaceString
+    internal class StringReplace
     {
         static void Main(string[] args)
         {
