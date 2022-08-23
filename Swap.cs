bde2bcc2a7bdbbf2f76b35a2cf0cd1a656e8457d
Manasa-1911 <89526344+Manasa-1911@users.noreply.github.com>
2022-08-23 21:52:39 +0530

    C# Day 2 Assignment

---
+++ Swap.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class swap
+    {
+        public static void SwapNo(ref int firstno, ref int secondno)
+        {
+            int temp;
+            temp = firstno;
+            firstno = secondno;
+            secondno = temp;
+        }
+
+        public static void Main()
+        {
+            int firstno = 5;
+            int secondno = 10;
+            Console.WriteLine($"Before Swapping a={firstno},b={secondno}");
+            SwapNo(ref firstno, ref secondno);
+            Console.WriteLine($"After Swapping a={firstno},b={secondno}");
+        }
+    }
+}
